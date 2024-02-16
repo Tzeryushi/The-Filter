1a92304435eef05f4bb54bd23d60743eac256c1d
Rhenn Handel <rwhandel@gmail.com>
2024-02-15 21:39:20 -0500

    basic movement moved to player (temp)

--- entities/entity_components/movement.gd
+++ entities/entity_components/movement.gd
@@ -1,34 +1,37 @@
 extends Node
-
-@export var speed: float = 3.0
-@export var acceleration: float = 30.0
-@export var gravity: float = 9.8
-
-@export var body: CharacterBody3D
-@export var camera: Camera3D
-
-var move_dir: Vector2
-var walk_velocity: Vector3
-var gravitational_velocity : Vector3
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+##Affects the movement of the player body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float):
-	body.velocity = _walk(delta) + _gravity(delta)
-	body.move_and_slide()
+#@export var speed: float = 3.0
+#@export var acceleration: float = 30.0
+#@export var gravity: float = 9.8
+#
+#@export var body: CharacterBody3D
+#@export var camera: Camera3D
+#
+#var move_dir: Vector2
+#var walk_velocity: Vector3
+#var gravitational_velocity : Vector3
 
-func _walk(delta: float) -> Vector3:
-	move_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	var forward: Vector3 = camera.global_transform.basis * Vector3(move_dir.x, 0, move_dir.y)
-	var walk_direction: Vector3 = Vector3(forward.x, 0, forward.z).normalized()
-	walk_velocity = walk_velocity.move_toward(walk_direction * speed * move_dir.length(), acceleration * delta)
-	return walk_velocity
 
-func _gravity(delta: float) -> Vector3:
-	gravitational_velocity = Vector3.ZERO if body.is_on_floor() else gravitational_velocity.move_toward(
-				Vector3(0, body.velocity.y - gravity, 0), gravity * delta)
-	return gravitational_velocity
+#func _ready():
+	#set_process(false)
+	#pass
+#
+#func _physics_process(delta: float):
+	#body.velocity = _walk(delta) + _gravity(delta)
+	#body.move_and_slide()
+#
+#
+#func _walk(delta: float) -> Vector3:
+	#move_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	#var forward: Vector3 = camera.global_transform.basis * Vector3(move_dir.x, 0, move_dir.y)
+	#var walk_direction: Vector3 = Vector3(forward.x, 0, forward.z).normalized()
+	#walk_velocity = walk_velocity.move_toward(walk_direction * speed * move_dir.length(), acceleration * delta)
+	#return walk_velocity
+#
+#
+#func _gravity(delta: float) -> Vector3:
+	#gravitational_velocity = Vector3.ZERO if body.is_on_floor() else gravitational_velocity.move_toward(
+				#Vector3(0, body.velocity.y - gravity, 0), gravity * delta)
+	#return gravitational_velocity
--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,38 +1,41 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export var speed: float = 3.0
+@export var acceleration: float = 30.0
+@export var gravity: float = 9.8
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = 9.8
+@export var camera: Camera3D
+
+var move_dir: Vector2
+var walk_velocity: Vector3
+var gravitational_velocity : Vector3
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	set_process(false)
 
 func _unhandled_input(event) -> void:
 	if event.is_action_pressed("esc"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
-#func _physics_process(delta):
-	## Add the gravity.
-	#if not is_on_floor():
-		#velocity.y -= gravity * delta
-#
-	## Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-#
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	#var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	#var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	#if direction:
-		#velocity.x = direction.x * SPEED
-		#velocity.z = direction.z * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-		#velocity.z = move_toward(velocity.z, 0, SPEED)
-#
-	#move_and_slide()
+
+func _physics_process(delta: float):
+	velocity = _walk(delta) + _gravity(delta)
+	move_and_slide()
+
+
+func _walk(delta: float) -> Vector3:
+	move_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var forward: Vector3 = camera.global_transform.basis * Vector3(move_dir.x, 0, move_dir.y)
+	var walk_direction: Vector3 = Vector3(forward.x, 0, forward.z).normalized()
+	walk_velocity = walk_velocity.move_toward(walk_direction * speed * move_dir.length(), acceleration * delta)
+	return walk_velocity
+
+
+func _gravity(delta: float) -> Vector3:
+	gravitational_velocity = Vector3.ZERO if is_on_floor() else gravitational_velocity.move_toward(
+			Vector3(0, velocity.y - gravity, 0), gravity * delta)
+	return gravitational_velocity
