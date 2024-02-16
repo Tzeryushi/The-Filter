c75321f873a00c430ac61ee999eeb0c447e9a7f1
Rhenn Handel <rwhandel@gmail.com>
2024-02-15 20:54:19 -0500

    gravity

--- entities/entity_components/movement.gd
+++ entities/entity_components/movement.gd
@@ -1,13 +1,15 @@
 extends Node
 
-@export var speed: float = 10.0
+@export var speed: float = 3.0
 @export var acceleration: float = 30.0
+@export var gravity: float = 9.8
 
 @export var body: CharacterBody3D
 @export var camera: Camera3D
 
 var move_dir: Vector2
 var walk_velocity: Vector3
+var gravitational_velocity : Vector3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,7 +18,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float):
-	body.velocity = _walk(delta)
+	body.velocity = _walk(delta) + _gravity(delta)
 	body.move_and_slide()
 
 func _walk(delta: float) -> Vector3:
@@ -25,3 +27,8 @@ func _walk(delta: float) -> Vector3:
 	var walk_direction: Vector3 = Vector3(forward.x, 0, forward.z).normalized()
 	walk_velocity = walk_velocity.move_toward(walk_direction * speed * move_dir.length(), acceleration * delta)
 	return walk_velocity
+
+func _gravity(delta: float) -> Vector3:
+	gravitational_velocity = Vector3.ZERO if body.is_on_floor() else gravitational_velocity.move_toward(
+				Vector3(0, body.velocity.y - gravity, 0), gravity * delta)
+	return gravitational_velocity
