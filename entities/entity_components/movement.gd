0c004f9cf887ccb3ebae845c70cc5988eefe6b87
Rhenn Handel <rwhandel@gmail.com>
2024-02-15 20:50:19 -0500

    working camera rotations

--- entities/entity_components/movement.gd
+++ entities/entity_components/movement.gd
@@ -1,5 +1,13 @@
 extends Node
 
+@export var speed: float = 10.0
+@export var acceleration: float = 30.0
+
+@export var body: CharacterBody3D
+@export var camera: Camera3D
+
+var move_dir: Vector2
+var walk_velocity: Vector3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,5 +15,13 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta: float):
+	body.velocity = _walk(delta)
+	body.move_and_slide()
+
+func _walk(delta: float) -> Vector3:
+	move_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var forward: Vector3 = camera.global_transform.basis * Vector3(move_dir.x, 0, move_dir.y)
+	var walk_direction: Vector3 = Vector3(forward.x, 0, forward.z).normalized()
+	walk_velocity = walk_velocity.move_toward(walk_direction * speed * move_dir.length(), acceleration * delta)
+	return walk_velocity
--- entities/player/player_camera.gd
+++ entities/player/player_camera.gd
@@ -1,6 +1,6 @@
 extends Camera3D
 
-var camera_sens: float = 1
+@export var camera_sensitivity: float = 1
 
 var look_dir: Vector2 # Input direction for look/aim
 
@@ -11,6 +11,6 @@ func _unhandled_input(event: InputEvent) -> void:
 		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 			_rotate_camera()
 
-func _rotate_camera(sens_mod: float = 1.0) -> void:
-	rotation.y -= look_dir.x * camera_sens * sens_mod
-	rotation.x = clamp(rotation.x - look_dir.y * camera_sens * sens_mod, -1.5, 1.5)
+func _rotate_camera(sensitivity_modifier: float = 1.0) -> void:
+	rotation.y -= look_dir.x * camera_sensitivity * sensitivity_modifier
+	rotation.x = clamp(rotation.x - look_dir.y * camera_sensitivity * sensitivity_modifier, -1.5, 1.5)
