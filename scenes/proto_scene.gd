48d618540583473eaa7cef4aa08d760534c3ff3b
Solomon <sol.lambert@gmail.com>
2024-02-15 22:21:39 -0600

    add test audio and add to proto_scene

--- scenes/proto_scene.gd
+++ scenes/proto_scene.gd
@@ -1,5 +1,15 @@
 extends Node3D
 
+var sound: AudioStream = preload("res://resources/sounds/sfx/take_me_to_your_leader.ogg")
+var sound_timer := Timer.new()
+func _ready():
+	add_child(sound_timer)
+	sound_timer.wait_time = .25
+	sound_timer.one_shot = true
+	sound_timer.start()
+	#sound_timer.timeout.connect(func(): AudioManager.play_sound(sound))
+	sound_timer.timeout.connect(func(): AudioManager.play_sound_at_location(sound, Vector3(10,0,10)))
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
