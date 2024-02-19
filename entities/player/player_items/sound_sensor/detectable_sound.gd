774a6b9ee096163d48de2df70b5ca20430a1c04b
Solomon <sol.lambert@gmail.com>
2024-02-19 17:37:23 -0600

    added detectable sounds and fixed animation players for monster and character model

--- entities/player/player_items/sound_sensor/detectable_sound.gd
+++
@@ -1,6 +0,0 @@
-class_name DetectableSound
-extends Area3D
-
-signal detected(sound: AudioStream)
-
-@export var sensor_sound_resource: AudioStream = preload("res://resources/sounds/sfx/take_me_to_your_leader.ogg")
--- entities/player/player_items/sound_sensor/player_sound_sensor.gd
+++ entities/player/player_items/sound_sensor/player_sound_sensor.gd
@@ -1,11 +1,15 @@
 extends PlayerItem
 
+
 @export var sound_on: AudioStream = preload("res://entities/player/player_items/flashlight/Flashlight on.wav")
 @export var sound_off: AudioStream = preload("res://entities/player/player_items/flashlight/Flashlight off.wav")
 @export var sound_default: AudioStream = preload("res://entities/player/player_items/sound_sensor/ScatterNoise1.mp3")
+@export var sound_sensor_ray: RayCast3D
 
 var active_stream_player: PrioritizedAudioStreamPlayer
 var is_on : bool = false : set = set_is_on
+var is_detected: bool = false
+
 
 func _ready() -> void:
 	is_on = false
@@ -14,21 +18,33 @@ func _ready() -> void:
 func on_exit() -> void:
 	stop_sensor_sound()
 	set_is_on(false)
+	sound_sensor_ray.enabled = false
 
-func _on_sound_detected(sound: AudioStream):
-	play_sensor_sound(sound)
 
-func _on_sound_undetected():
-	play_sensor_sound(sound_default)
+func _on_sound_detected(detected: DetectableSound):
+	if !is_detected and is_on:
+		is_detected = true
+		play_sensor_sound(detected.sensor_sound)
+
+
+func _on_sound_detections_ceased():
+	is_detected = false
+	if is_on:
+		play_sensor_sound(sound_default)
+
 
 func use() -> void:
 	is_on = !is_on
 	if is_on:
 		AudioManager.play_sound(sound_on)
 		play_sensor_sound(sound_default)
+		sound_sensor_ray.set_enabled(true)
 	else:
 		AudioManager.play_sound(sound_off)
+		sound_sensor_ray.set_enabled(false)
+		is_detected = false
 		stop_sensor_sound()
+
 
 func play_sensor_sound(sound: AudioStream):
 	if !active_stream_player:
@@ -39,11 +55,14 @@ func play_sensor_sound(sound: AudioStream):
 		active_stream_player.play()
 	if !active_stream_player.is_connected("finished", active_stream_player.play):
 		active_stream_player.finished.connect(active_stream_player.play)
-		
+
+
 func stop_sensor_sound():
 	if active_stream_player:
-		active_stream_player.finished.disconnect(active_stream_player.play)
+		if active_stream_player.is_connected("finished", active_stream_player.play):
+			active_stream_player.finished.disconnect(active_stream_player.play)
 		active_stream_player.stop()
+
 
 func set_is_on(value:bool) -> void:
 	is_on = value
