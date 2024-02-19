12d6278c8d5727ef69f29dbc2da76c34da686b7d
Rhenn Handel <rwhandel@gmail.com>
2024-02-19 17:28:43 -0500

    refactor client resource for dictionary

--- entities/clients/client.gd
+++ entities/clients/client.gd
@@ -1,3 +1,4 @@
 class_name Client
 extends Node
 
+var client_resource : ClientResource
--- entities/clients/client_resource.gd
+++ entities/clients/client_resource.gd
@@ -21,6 +21,7 @@ enum Threat {
 	"answered_age" : false,
 	"answered_job" : false,
 	"answered_reason" : false,
+	"answered_phenom" : false,
 }
 #@export var model
 #@export var skin
@@ -28,25 +29,45 @@ enum Threat {
 # SOME levels of symptoms are okay. Too many indicates a threat.
 
 @export_group("Benign Symptoms")
-@export_subgroup("Dialogue Symptoms")
-@export var forgetful: bool	## Affects dialogue, forgets details like name, age, background and trip reason
-@export var headache: bool	## Will complain about headache.
-@export var irrational: bool	## Will bring up non-sequitirs, ignore questions
-@export var nauseated: bool	## Will bring up nausea.
-@export var knows_too_much: bool	## This guy knows my name?
-@export var unresponsive: bool	## Ignores questions. Never a good sign.
-@export var hallucinating: bool	## Answers questions you didn't ask.
-@export var too_polite: bool	## Can't trust this guy.
-@export var encountered_entity: bool	## I seen it I swears
+#@export_subgroup("Dialogue Symptoms")
+@export var dialogue_symptoms: Dictionary = {
+	"forgetful": false,	## Affects dialogue, forgets details like name, age, background and trip reason
+	"headache": false,	## Will complain about headache.
+	"irrational": false,	## Will bring up non-sequitirs, ignore questions
+	"nauseated": false,	## Will bring up nausea.
+	"knows_too_much": false,	## This guy knows my name?
+	"unresponsive": false,	## Ignores questions. Never a good sign.
+	"hallucinating": false,	## Answers questions you didn't ask.
+	"too_polite": false,	## Can't trust this guy.
+	"encountered_entity": false,	## I seen it I swears
+}
+#@export var forgetful: bool	## Affects dialogue, forgets details like name, age, background and trip reason
+#@export var headache: bool	## Will complain about headache.
+#@export var irrational: bool	## Will bring up non-sequitirs, ignore questions
+#@export var nauseated: bool	## Will bring up nausea.
+#@export var knows_too_much: bool	## This guy knows my name?
+#@export var unresponsive: bool	## Ignores questions. Never a good sign.
+#@export var hallucinating: bool	## Answers questions you didn't ask.
+#@export var too_polite: bool	## Can't trust this guy.
+#@export var encountered_entity: bool	## I seen it I swears
 
-@export_subgroup("Environment Symptoms")
-@export var strange_growths: bool	## Can be benign.
-@export var averse_to_light: bool	## Usually nothing. Can be a shadowhiker.
-@export var time_dilation: bool	## Clock speed is affected at random intervals.
-@export var too_many_heartbeats: bool	## Sometimes a pulse jockey. Sometimes people are just weird.
-@export var second_presence: bool	## Feels like someone else is around.
-@export var ineplicable_phenomena: bool	## Something's wrong.
-@export var manifesting_aura: bool	## A forboding aura emanates. A lingering spirit?
+#@export_subgroup("Environment Symptoms")
+@export var env_symptoms: Dictionary = {
+	"strange_growths": false,	## Can be benign.
+	"averse_to_light": false,	## Usually nothing. Can be a shadowhiker.
+	"time_dilation": false,	## Clock speed is affected at random intervals.
+	"too_many_heartbeats": false,	## Sometimes a pulse jockey. Sometimes people are just weird.
+	"second_presence": false,	## Feels like someone else is around.
+	"ineplicable_phenomena": false,	## Something's wrong.
+	"manifesting_aura": false,	## A forboding aura emanates. A lingering spirit?
+}
+#@export var strange_growths: bool	## Can be benign.
+#@export var averse_to_light: bool	## Usually nothing. Can be a shadowhiker.
+#@export var time_dilation: bool	## Clock speed is affected at random intervals.
+#@export var too_many_heartbeats: bool	## Sometimes a pulse jockey. Sometimes people are just weird.
+#@export var second_presence: bool	## Feels like someone else is around.
+#@export var ineplicable_phenomena: bool	## Something's wrong.
+#@export var manifesting_aura: bool	## A forboding aura emanates. A lingering spirit?
 
 
 func _ready() -> void:
