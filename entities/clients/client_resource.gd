3eb9375579d447c1c8fc11eeff7067eac0ff0746
Rhenn Handel <rwhandel@gmail.com>
2024-02-21 03:31:32 -0500

    updated threat definitions

--- entities/clients/client_resource.gd
+++ entities/clients/client_resource.gd
@@ -5,7 +5,7 @@ extends Resource
 enum Personality {NORMAL = 0, GRUMPY = 1, IMPATIENT = 2, QUIET = 3, STOIC = 4, DERANGED = 5, ENTHUSIASTIC = 6, LIAR = 7,}
 enum Threat {
 	SAFE = 0, OVERSYMPTOMATIC = 1, SHADOWHIKER = 2, FLESHRIDER = 3, DOPPELGANGER = 4,
-	PULSEJOCKEY = 5, CHRONOMONON = 6, LINGERING_SPIRIT = 7, UNKNOWN = 8}
+	PULSEJOCKEY = 5, CHRONOMONON = 6, DISCARNIVORE = 7, UNKNOWN = 8}
 
 @export var client_name: String
 @export var client_age: int
@@ -63,6 +63,12 @@ enum Threat {
 @export var use_override: bool = false
 @export var size_scale: float = 0.8
 
+
+var threat_dict: Dictionary = {
+	Threat.OVERSYMPTOMATIC : oversymptomatic,
+}
+
+
 func _ready() -> void:
 	dialogue_state["client_name"] = client_name
 	dialogue_state["client_age"] = client_age
@@ -70,3 +76,54 @@ func _ready() -> void:
 	dialogue_state["travel_reason"] = travel_reason
 	dialogue_state["personality_type"] = personality_type
 	dialogue_state["threat_type"] = threat_type
+
+
+func oversymptomatic() -> void:
+	pass
+	
+
+func shadowhiker() -> void:
+	dialogue_symptoms["headaches"] = true
+	dialogue_symptoms["knows_too_much"] = true
+	env_symptoms["averse_to_light"] = true
+	env_symptoms["increased_shadows"] = true
+
+
+func fleshrider() -> void:
+	dialogue_symptoms["irrational"] = true
+	dialogue_symptoms["nauseated"] = true
+	dialogue_symptoms["hallucinating"] = true
+	env_symptoms["strange_growths"] = true
+
+
+func chronomonon() -> void:
+	dialogue_symptoms["hallucinating"] = true
+	dialogue_symptoms["knows_too_much"] = true
+	env_symptoms["time_dilation"] = true
+	env_symptoms["manifesting_aura"] = true
+	
+
+func doppelganger() -> void:
+	dialogue_symptoms["too_polite"] = true
+	dialogue_symptoms["forgetful"] = true
+	dialogue_symptoms["irrational"] = true
+	env_symptoms["crickets"] = true
+	
+
+func pulsejockey() -> void:
+	dialogue_symptoms["unresponsive"] = true
+	dialogue_symptoms["headache"] = true
+	dialogue_symptoms["forgetful"] = true
+	env_symptoms["too_many_heartbeats"] = true
+
+
+func disincarnivores() -> void:
+	dialogue_symptoms["hallucinating"] = true
+	dialogue_symptoms["unresponsive"] = true
+	env_symptoms["second_presence"] = true
+	env_symptoms["strange_sounds"] = true
+	
+
+func unknown() -> void:
+	pass
+	
