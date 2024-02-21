2ad001ab7391b17bf6fdc9fb6262d67fc3d7f543
Solomon <sol.lambert@gmail.com>
2024-02-20 21:10:42 -0600

    added a lot of sound effects, ambient music, multiple heartbeats

--- assets/detectable_sounds/detectable_sound.gd
+++ assets/detectable_sounds/detectable_sound.gd
@@ -6,11 +6,12 @@ extends Area3D
 @export var crickets_sound: AudioStream
 @export var ghosts_sound: AudioStream
 @export var heartbeat_sound: AudioStream
+@export var multiple_heartbeat_sound: AudioStream
 @export var scream_sound: AudioStream
 
 var active_sound: AudioStream
 
-enum SoundType {CREEPY, CRICKETS, GHOSTS, HEARTBEAT, SCREAM}
+enum SoundType {CREEPY, CRICKETS, GHOSTS, HEARTBEAT, MULTIPLE_HEARTBEAT, SCREAM}
 
 
 func _ready():
@@ -27,5 +28,7 @@ func set_sensor_sound(new_type: SoundType):
 			active_sound = ghosts_sound
 		SoundType.HEARTBEAT:
 			active_sound = heartbeat_sound
+		SoundType.MULTIPLE_HEARTBEAT:
+			active_sound = multiple_heartbeat_sound
 		SoundType.SCREAM:
 			active_sound = scream_sound
