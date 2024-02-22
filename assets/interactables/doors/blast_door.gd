0fc6069a3bf05735d43bf576d3dee2ff88b1d30b
Rhenn Handel <rwhandel@gmail.com>
2024-02-21 19:54:18 -0500

    Working computer, exit doors

--- assets/interactables/doors/blast_door.gd
+++ assets/interactables/doors/blast_door.gd
@@ -10,9 +10,11 @@ func _ready():
 	if is_open:
 		open_door()
 
+
 func open_door():
 	is_open = true
 	animation_player.play("door_open")
+
 
 func close_door():
 	is_open = false
@@ -26,9 +28,24 @@ func toggle() -> void:
 		close_door()
 
 
+func set_door_open(state: bool) -> void:
+	if state:
+		open_door()
+	else:
+		close_door()
+
+
 func _on_small_button_pressed():
 	toggle()
 
 
 func _on_env_player_entered(_player):
 	toggle()
+
+
+func _on_area_body_entered(body):
+	open_door()
+
+
+func _on_area_3d_body_exited(body):
+	close_door()
